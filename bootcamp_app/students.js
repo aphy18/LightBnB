11b05ccc82bb146017c8f17eefbc47725067d392
aphy18 <aphason1@.com>
2021-08-19 21:00:58 +0000

    dealt with malicious users

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,8 +1,5 @@
 const { Pool } = require('pg');
 
-const cohortName = process.argv[2];
-const numberOfResults = process.argv[3];
-
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
@@ -10,16 +7,22 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const cohortName = process.argv[2] || 'JUL02';
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+const queryString = pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${cohortName}}%'
-LIMIT ${numberOfResults || 5};
+WHERE cohorts.name LIKE $1
+LIMIT $2;
 `)
+pool.query(queryString, values);
   .then(res => {
-    res.rows.forEach(user => {
-      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    res.rows.forEach(row => {
+      console.log(`${row.name} has an id of ${row.student_id} and was in the ${row.cohort} cohort`);
+      console.log(res.rows);
     });
-  })
-  .catch(err => console.error('query error', err.stack));
+  });
